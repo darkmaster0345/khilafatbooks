b7a1bae16adb757e51086c1d8f532638dba91e1c
Dark Master <arifubaid0345@gmail.com>
2026-04-20 16:27:23 +0500

    chore: remove obsolete GoogleReCaptchaProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from 'react-helmet-async';
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import App from "./App.tsx";
 import "./index.css";
-
-const RECAPTCHA_SITE_KEY = import.meta.env.VITE_RECAPTCHA_SITE_KEY || "";
 
 const container = document.getElementById("root")!;
 
 const Main = () => (
   <HelmetProvider>
-    <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
-      <App />
-    </GoogleReCaptchaProvider>
+    <App />
   </HelmetProvider>
 );
 
